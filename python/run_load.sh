e1ff60d81171c5f6ed7b659b7961c017b57e6879
Pavan Kumar Paluri <pvpk1994@gmail.com>
2019-05-04 12:04:10 -0500

    Parallelization enabled, timer support - just to note the runtime

--- python/run_load.sh
+++ python/run_load.sh
@@ -1,7 +1,23 @@
+
+#!/bin/bash
+#SBATCH -J SIM_RRP
+#SBATCH -o SIM_RRP.o%j
+#SBATCH -t 15:00:00
+#SBATCH -N 1 -n 10
+#SBATCH -A amcheng
+
+# Inclusion of timer to see the time elapsed
+START=$(date +%s)
+
 loads=(0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0)
 
 for load in ${loads[@]};
 do
-	echo $load
-	sbatch run.sh $load
+  	echo $load
+        # Enable Parallelization
+        sbatch run.sh $load &
 done
+END=$(date +%s)
+echo $?
+DIFF=$(( END - START ))
+echo "Time Elapsed: $DIFF seconds"
